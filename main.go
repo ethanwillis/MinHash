3d51ebdbce1d7a74bb0315dbddc19f96f928cfd3
vtphan <vphan@memphis.edu>
2017-10-11 19:57:18 -0500

    .

--- main.go
+++ main.go
@@ -10,18 +10,20 @@ func main() {
 	k, window_size, overlap := 7, 100, 10
 	h := NewHash(k, window_size, overlap)
 	for i := 0; i < 10; i++ {
+		fmt.Println("Hash random dna of length", N, "with id", i)
 		dna := random_dna(N)
 		h.Hash(dna, i)
 	}
 	// h.Print()
+	fmt.Println("Look up a few keys.")
 	i := 0
 	for k, _ := range h.Table {
-		fmt.Println(k, h.Lookup(k))
+		fmt.Println("Key:", k, "Value:", h.Lookup(k))
 		i++
 		if i == 10 {
 			break
 		}
 	}
 	key := HashKey{0, 1, 2}
-	fmt.Println(h.Lookup(key))
+	fmt.Println("Key:", key, "Value:", h.Lookup(key))
 }
